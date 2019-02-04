484482d9f9bf35322691b5a6ff826f3e77c8488a
korabelus <korabel7777@gmail.com>
2019-02-04 23:21:28 +0200

    forth step

--- car.rb
+++ car.rb
@@ -1,13 +1,24 @@
 class Car
   attr_accessor :color
+  attr_reader :drivers_names
+
 
   def initialize(number)
   	@number = number
   	@color = 'white'
+  	@drivers_names = []
   end
 
   def beep
     puts "beep, beep"
+  end
+
+  def add_driver(name)
+  	@drivers_names << name
+  end
+
+  def show_drivers
+  	@drivers_names.each {|name| puts name}
   end
 
 end
