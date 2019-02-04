c951afa7e4cfa32fc8d0f9f2b3034e1bddfed4c0
korabelus <korabel7777@gmail.com>
2019-02-04 23:11:34 +0200

    third step

--- car.rb
+++ car.rb
@@ -2,9 +2,19 @@ class Car
 
   def initialize(number)
   	@number = number
+  	@color = 'white'
   end
 
   def beep
     puts "beep, beep"
   end
+
+  def color=(color)
+  	@color = color
+  end
+
+  def color
+  	@color
+  end
+  
 end
